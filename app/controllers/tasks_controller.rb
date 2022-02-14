bb16e9a6cb9ee7f03cc61e3ae6823a702a4661ef
Smail Liams <b.smailiams@gmail.com>
2022-02-14 14:41:40 +0100

    Mark as complete ability for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,19 @@
 class TasksController < ApplicationController
 
     before_action :set_project
+    before_action :set_task, only: [:complete,:destroy]
 
     def create
         @task = @project.tasks.create(task_params)
-    
         redirect_to @project 
     end
 
+    def complete
+        @task.update_attribute(:completed_at, Time.now)
+        redirect_to @project, notice: "Task Completed"
+    end
+
     def destroy
-        @task = @project.tasks.find(params[:id]) 
         if @task.destroy 
             flash[:success] = "Task was deleted."
         else  
@@ -22,6 +26,10 @@ class TasksController < ApplicationController
 
     def set_project
         @project = Project.find(params[:project_id])
+    end
+
+    def set_task
+        @task = @project.tasks.find(params[:id]) 
     end
 
     def task_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_14_110025) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_14_124422) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_14_110025) do
     t.integer "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
